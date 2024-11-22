6221a3bcc33573d7a843c08234a61193bcc1115d
Selassie Kwaku Davids <seladavids90@gmail.com>
2024-11-22 18:43:05 +0000

    feat: add prompt for user feat: design webpage and style it
    A prompt for the user to make a guess was added
    
    Text was added to the html page to provide the user with some information.
    The page was also styled.

--- script.js
+++ script.js
@@ -13,4 +13,28 @@ function getComputerChoice() {
     return result;
 }
 
-console.log (getComputerChoice());
+/* write a function that takes human choice and returns it. The choice has to be 
+made case-insensitive. */
+function getHumanChoice () {
+    let choice = prompt("Enter guess: rock, paper, or scissors");
+    //make it case-insensitive by making all characters lowercase
+    let humanChoice = choice.toLowerCase();
+    //create a check for invalid choices
+    while (humanChoice !== "rock" &&
+           humanChoice !== "paper" &&
+           humanChoice !== "scissors" 
+    ) {
+        alert("Enter a valid choice");
+        choice = prompt("Enter guess: rock, paper, or scissors");
+        humanChoice = choice.toLowerCase();
+    }
+    return humanChoice;
+}
+
+//variables to keep track of the players' score
+let humanScore = 0;
+let computerScore = 0;
+
+/* write a function that takes the human and computer player choices as arguments, 
+plays a single round, increments the round winner’s score and logs a winner 
+announcement.*/
